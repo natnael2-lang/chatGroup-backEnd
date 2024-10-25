1ca01c135d85f37fe0919b62b9e2610a746d5795
Natnael1-lang <natnaelmessay70@gmail.com>
2024-10-25 21:55:09 +0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const session = require('express-session');
-const routerTodoList = require("./Router/toDoData");
-const routerUser = require("./Router/user");
 const bodyParser = require('body-parser');
 const db = require("./Connector/db");
 const cors = require('cors');
@@ -13,62 +10,30 @@ const Data = require("./Model/data");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to the database
+
 db().then(() => {
     console.log('Database connected');
 });
 
-// Middleware setup
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
-    secret: 'nat-mes12',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false, // Set to true if using HTTPS
-        maxAge: 900000 // 15 minutes
-    }
-}));
 
-const checkCookieAndRedirect = (req, res, next) => {
-    if (req.session.userId || req.cookies.username) {
-        return res.redirect('/home');
-    } else {
-        return res.redirect('/login');
-    }
-};
 
-// Uncomment these routes if needed
-/*
-app.get('/', checkCookieAndRedirect);
-app.get('/home', (req, res) => {
-    res.send("Welcome to the Homepage!");
-});
-app.get('/login', (req, res) => {
-    res.send("Please log in!");
-});
-*/
-
-// Uncomment these if you are using these routers
-// app.use("/data", routerTodoList);
-// app.use("/user", routerUser);
-
-// Start the server
 const server = app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// WebSocket server setup
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     console.log('New client connected');
 
     ws.on('message', (message) => {
-        // Broadcast incoming message to all connected clients
+        
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
@@ -81,7 +46,7 @@ wss.on('connection', (ws) => {
     });
 });
 
-// Endpoint to create a new task
+
 app.post("/data", async (req, res) => {
     try {
         const newData = await Data.create(req.body);
@@ -90,32 +55,32 @@ app.post("/data", async (req, res) => {
             task: newData
         });
 
-        // Broadcast new task to all connected WebSocket clients
+        
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(taskMessage);
             }
         });
 
-        res.status(201).json(newData); // Respond with the created task
+        res.status(201).json(newData); 
     } catch (error) {
         console.error("Error creating task:", error);
-        res.status(400).json({ error: error.message }); // Respond with an error message
+        res.status(400).json({ error: error.message }); 
     }
 });
 
-// Endpoint to get all tasks
+
 app.get("/data", async (req, res) => {
     try {
         const data = await Data.find();
-        res.status(200).json(data); // Use status 200 for successful GET requests
+        res.status(200).json(data); 
     } catch (error) {
         console.error("Failed to get all todo data:", error);
         res.status(500).send('Server error');
     }
 });
 
-// Endpoint to update a task
+
 app.patch("/data/:id", async (req, res) => {
     try {
         const id = req.params.id;
